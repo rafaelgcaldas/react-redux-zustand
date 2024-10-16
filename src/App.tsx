99683bf1ce3f9b799c9da36c75b4c91323e5bdb7
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-15 21:25:22 -0300

    feat: Player page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Provider as ReduxProvider } from "react-redux";
+import { Player } from "./pages/player";
 import { store } from "./store";
+
+import './styles/global.css';
 
 export function App() {
   return (
     <div>
       <ReduxProvider store={store}>
-        <h1>teste</h1>
+        <Player />
       </ReduxProvider>
     </div>
   )
