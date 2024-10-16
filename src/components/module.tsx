021e883297ae5687a20407f50fe3091b25082f78
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-16 12:11:56 -0300

    feat: show current lesson

--- src/components/module.tsx
+++ src/components/module.tsx
@@ -16,6 +16,12 @@ interface ModuleProps {
 export function Module({ title, amountOfLessons, moduleIndex }: ModuleProps) {
   const dispatch = useDispatch()
 
+  const { currentModuleIndex, currentLessonIndex } = useAppSelector(state => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+
+    return { currentModuleIndex, currentLessonIndex }
+  })
+
   const lessons = useAppSelector(state => {
     return state.player.course.modules[moduleIndex].lessons
   })
@@ -36,14 +42,19 @@ export function Module({ title, amountOfLessons, moduleIndex }: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson, lessonIndex) => (
+          {lessons.map((lesson, lessonIndex) => {
+            const iscurrent = currentModuleIndex === moduleIndex && currentLessonIndex === lessonIndex
+          
+            return (
               <Lesson 
                 key={lesson.id} 
                 title={lesson.title} 
                 duration={lesson.duration}
                 onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+                isCurrent={iscurrent}
               />
-            ))}
+            )
+          })}
         </nav>
       </Collapsible.Content>
     </Collapsible.Root>
