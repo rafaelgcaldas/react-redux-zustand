d9c546907c1828ca474a78189394a4af49aea206
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-16 15:38:33 -0300

    feat: add autoplay

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,20 +1,29 @@
 import ReactPlayer from 'react-player'
 import { useAppSelector } from '../store'
+import { useDispatch } from 'react-redux'
+import { next } from '../store/slices/player'
 
 export function VideoPlayer () {
+  const dispatch = useDispatch()
+
   const lesson = useAppSelector(state => {
     const { currentModuleIndex, currentLessonIndex} = state.player
-
     const currentLesson = state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
 
     return currentLesson
   })
+
+  function handlePlayNext() {
+    dispatch(next())
+  }
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         width="100%"
         height="100%"
         controls
+        onEnded={handlePlayNext}
         url={`https://www.youtube.com/watch?v=${lesson.id}`}
       />
     </div>
